aebd0d2a476b9b020b8161076df7d24c6941bb1a
Yousef emad mikhail <mad33833@gmail.com>
2025-04-14 00:29:06 +0200

    add display function to display recent orders

--- js/script.js
+++ js/script.js
@@ -28,6 +28,7 @@ const sideMenu=document.querySelector("aside")
 const menuBtn=document.querySelector("#menu-btn")
 const closeBtn=document.querySelector("#close-btn")
 const themeToggler=document.querySelector(".theme-toggler")
+const tableContent=document.querySelector('table tbody')
 
 menuBtn.addEventListener('click',()=>{
     sideMenu.style.display="block"
@@ -40,3 +41,85 @@ themeToggler.addEventListener('click',()=>{
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 })
+
+
+
+const orders = [
+    {
+      productName: "Wireless Headphones",
+      number: 1054,
+      payment: "Paid",
+      status: "Delivered"
+    },
+    {
+      productName: "Bluetooth Speaker",
+      number: 658,
+      payment: "Due",
+      status: "Pending"
+    },
+    {
+      productName: "Smartwatch",
+      number: 4231,
+      payment: "Refund",
+      status: "Canceled"
+    },
+    {
+      productName: "Gaming Mouse",
+      number: 112,
+      payment: "Paid",
+      status: "Delivered"
+    },
+    {
+      productName: "Laptop Stand",
+      number: 987,
+      payment: "Due",
+      status: "Pending"
+    },
+    {
+      productName: "Mechanical Keyboard",
+      number: 2045,
+      payment: "Paid",
+      status: "Delivered"
+    },
+    {
+      productName: "USB-C Cable",
+      number: 332,
+      payment: "Refund",
+      status: "Canceled"
+    },
+    {
+      productName: "Portable SSD",
+      number: 775,
+      payment: "Paid",
+      status: "Delivered"
+    },
+    {
+      productName: "Noise Cancelling Earbuds",
+      number: 1400,
+      payment: "Due",
+      status: "Pending"
+    },
+    {
+      productName: "Webcam",
+      number: 351,
+      payment: "Paid",
+      status: "Delivered"
+    }
+  ];
+
+function display (){
+    let cartona=``
+    orders.forEach((order)=>{
+        cartona+=`
+        <tr>
+            <td>${order.productName}</td>
+            <td>${order.number}</td>
+            <td>${order.payment}</td>
+            <td class="${order.status==="Canceled"?"danger":order.status==="Delivered"?"success":"warning"}">${order.status}</td>
+            <td class="primary">Details</td>
+        </tr>
+        `
+    })
+    tableContent.innerHTML=cartona
+}
+display()
